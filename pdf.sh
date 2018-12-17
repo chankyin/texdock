fde2f198de33dc5e71540cb08b1acea55e5e8229
chankyin <chankyin1970@gmail.com>
2018-12-17 16:02:35 +0800

    pdflatex needs to be run twice
    https://tex.stackexchange.com/a/301109

--- pdf.sh
+++ pdf.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-for file in /input/*.tex
+cp /input/*.tex .
+for file in *.tex
 do
 	echo === RUN: pdflatex "$file" ===
+	pdflatex "$file"
+	echo === RE-RUN: pdflatex "$file" === # because https://tex.stackexchange.com/a/301109
 	pdflatex "$file"
 done
