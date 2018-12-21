fcaf39058969a7ff44829314f7d205b841fa7f05
chankyin <chankyin1970@gmail.com>
2018-12-22 02:02:12 +0800

    Install make

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y tzdata texlive-latex-recommended texlive-fonts-extra
-
+apt-get install -y tzdata texlive-latex-recommended texlive-fonts-extra make
